a7ebe77e4853d4aa6abbba291f79a4553ae8e38c
yyh <646167686@qq.com>
2018-07-11 13:16:47 +0800

    新建仓库

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+int main() {
+    Mat myMat = imread("lena.jpg");
+
+    return 0;
+}
